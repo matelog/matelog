ce36f2edb241ced20233ae925be1c1bacec7b247
matelog <98547607+matelog@users.noreply.github.com>
2025-10-02 12:20:20 -0600

    Add files via upload

---
+++ matelog/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for matelog project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'matelog.settings')
+
+application = get_asgi_application()
